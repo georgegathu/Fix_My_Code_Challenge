090315f9cb35ad7f965f8879afdcd610a568ef7d
georgegathu <ggeorge.gathu@gmail.com>
2023-04-14 16:37:14 +0300

    1. My square

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+	The code problem is that the area_of_my_square method is calculating the area of the square incorrectly.
+	It should multiply the width by the height instead of width by width. 
+	Also, the naming convention of the PermiterOfMySquare method violates the Python naming conventions; 
+	it should be renamed to perimeter_of_my_square to follow the PEP8 guidelines.
+"""
+
+class Square():
+    
+    width = 0
+    height = 0
+
+    
+    def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.height
+
+    def perimeter_of_my_square(self):
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.perimeter_of_my_square())
